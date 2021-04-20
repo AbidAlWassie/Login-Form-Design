39f87e737f550b2205289b16950fa93c6b5ddb7b
Abid Al-Wasi <abidalwasi@outlook.com>
2021-04-20 20:00:26 +0600

    updated project

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
 window.onload = () => {
-  let toggleBtn = document.querySelector(".btn");
-  toggleBtn.addEventListener("click", toggleNav);
-  
-  function toggleNav() {
-    const formSignIn = document.querySelector(".form-signin");
-    const formSignUp = document.querySelector(".form-signup");
+
+  let signinBtn = document.querySelector(".signinBtn");
+  let signupBtn = document.querySelector(".signupBtn");
+
+  signinBtn.addEventListener("click", signin);
+  signupBtn.addEventListener("click", signup);
+
+  const signupInactive = document.querySelector(".one");
+  const signinActive = document.querySelector(".two");
+
+  const forgotPassword = document.querySelector(".forgot");
+
+  const formSignIn = document.querySelector(".form-signin");
+  const formSignUp = document.querySelector(".form-signup");
+
+  const frame = document.querySelector(".frame");
+
+  function signin() {
+    forgotPassword.classList.remove("forgot-left");
+    frame.classList.remove("frame-long");
+    formSignIn.classList.remove("form-signin-left");
+    formSignUp.classList.remove("form-signup-left");
+    signinActive.classList.remove("signup-active");
+    signinActive.classList.add("signin-inactive");
+    signupInactive.classList.remove("signin-inactive");
+    // console.log("Sign In");
+  }
+
+  function signup() {
+    forgotPassword.classList.add("forgot-left");
+    frame.classList.add("frame-long");
+    formSignIn.classList.add("form-signin-left");
+    formSignUp.classList.add("form-signup-left");
+    signinActive.classList.add("signup-active");
+    signinActive.classList.remove("signup-inactive");
+    signupInactive.classList.add("signin-inactive");
+    console.log("Sign Up");
   }
 }
 
